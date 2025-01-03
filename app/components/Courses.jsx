57bf6bf649b5768c473b72c8d1f927d0649de3cb
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-03 15:22:13 -0500

    added Courses page

---
+++ app/components/Courses.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+// async function fetchCourses() {
+//   const response = await fetch("http://localhost:3000/api/courses");
+
+//   const courses = await response.json(); // Extract JSON data
+//   return courses;
+// }
+
+const Courses = ({ courses }) => {
+  // const courses = await fetchCourses();
+  // console.log(courses);
+  return (
+    <div className="courses">
+      {courses.map((course) => (
+        <div key={course.id} className="card">
+          <h3>{course.title}</h3>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go To Course
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Courses;
