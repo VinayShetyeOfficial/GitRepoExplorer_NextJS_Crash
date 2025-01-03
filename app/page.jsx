9e4f8279004d40cc559a184c132c97ab642b703a
Vinay Shetye <146690087+OurUMassd-PROJECTS@users.noreply.github.com>
2025-01-02 23:29:31 -0500

    app router

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome to Traversy Media</h1>
+      <ul>
+        <li>
+          <Link href="/Home">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/team">Team</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
