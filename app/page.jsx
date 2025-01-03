31f324c75d9b8a9fdf1f2f63849e6f67e27e9db6
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-03 15:24:19 -0500

    updated index/main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,35 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import LoadingPage from "./loading";
+import Courses from "./components/Courses";
+import CourseSearch from "./components/CourseSearch";
 
 const page = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
       <h1>Welcome to Traversy Media</h1>
-      <ul>
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
+      {/* <ul>
         <li>
           <Link href="/Home">Home</Link>
         </li>
@@ -14,7 +39,7 @@ const page = () => {
         <li>
           <Link href="/about/team">Team</Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
