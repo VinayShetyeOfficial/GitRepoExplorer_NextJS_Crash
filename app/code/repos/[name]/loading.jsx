f9afbcf9376c8f86cca52b7b09b9a6749da62847
Vinay Shetye <146690087+OurUMassd-PROJECTS@users.noreply.github.com>
2025-01-02 23:41:04 -0500

    repo loading

---
+++ app/code/repos/[name]/loading.jsx
@@ -0,0 +1,12 @@
+// In Next.js, the loading.jsx file is a special file with built-in support or
+// rendering a loading state during route transitions or data fetching. Here's how it works
+
+const LoadingPage = () => {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingPage;
