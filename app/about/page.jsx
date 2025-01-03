ccea4d0764b0c751e026505100998570c708649f
Vinay Shetye <146690087+OurUMassd-PROJECTS@users.noreply.github.com>
2025-01-02 23:30:03 -0500

    about router

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "About Traversy Media",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy media</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet
+        repellendus iusto molestiae nesciunt commodi eos nisi culpa. Quis
+        quaerat possimus minus, consequatur sint tenetur porro nemo? Quos, minus
+        ab mollitia distinctio magnam eius saepe rem eligendi voluptates
+        laboriosam eaque reprehenderit hic nisi quis harum libero nihil! Quidem
+        magnam ipsa sunt?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eum,
+        perspiciatis eaque maiores unde saepe molestias laudantium expedita
+        similique repudiandae! Omnis repellendus autem hic similique minima,
+        voluptates cupiditate suscipit. Illo accusantium voluptatum voluptatibus
+        consequatur minus possimus cupiditate laborum mollitia veniam, enim eos
+        quod placeat cumque inventore eveniet ex dolore dolorum.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
